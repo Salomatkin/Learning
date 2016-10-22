125e5735fbf2992d8efba9d24a0aa5496b8e9b66
Salomatkin <salomatkin@inbox.ru>
2016-10-22 23:53:39 +1000

    Add Function

--- MySnake/MySnake/Program.cs
+++ MySnake/MySnake/Program.cs
@@ -10,6 +10,21 @@ namespace MySnake
     {
         static void Main(string[] args)
         {
+            int x1 = 5;
+            int y1 = 3;
+            char sym1 = '&';
+
+            Draw(x1, y1, sym1);
+
+            Console.Read();
+        }
+
+
+
+        static void Draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
         }
     }
 }
