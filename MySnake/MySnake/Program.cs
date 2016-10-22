8c899f22d13fa064ffaff9557bbda2fb33d28c4a
Salomatkin <salomatkin@inbox.ru>
2016-10-23 00:04:03 +1000

    some

--- MySnake/MySnake/Program.cs
+++ MySnake/MySnake/Program.cs
@@ -11,16 +11,13 @@ namespace MySnake
         static void Main(string[] args)
         {
             int x1 = 5;
-            int y1 = 3;
+            int y1 = 6;
             char sym1 = '&';
 
             Draw(x1, y1, sym1);
 
             Console.Read();
         }
-
-
-
         static void Draw(int x, int y, char sym)
         {
             Console.SetCursorPosition(x, y);
