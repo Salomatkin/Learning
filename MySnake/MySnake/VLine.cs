d9a73f28e0bf3e630deee2a2a6cace03421d4d7b
Salomatkin <salomatkin@inbox.ru>
2016-10-26 00:09:33 +1000

    Lines

---
+++ MySnake/MySnake/VLine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySnake
+{
+    class VLine
+    {
+        List<Point> pList;
+        public VLine(int x, int y1, int y2, char sym)
+        {
+            pList = new List<Point>();
+            for (int y = y1; y <= y2; y++)
+            {
+                pList.Add(new Point(x, y, sym));
+            }
+        }
+        public void drw()
+        {
+            foreach (Point p in pList)
+            {
+                p.Draw();
+            }
+        }
+
+    }
+}
